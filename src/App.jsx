fac4491232b7f9b73eced5b08973af17ca2e7597
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-05-24 18:36:51 -0500

    iniciamos

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { useState } from 'react'
+
+import './App.css'
+import Home from './modules/Home'
+import Pokedex from './modules/Pokedex'
+
+function App() {
+ 
+
+  return (
+
+<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/pokedex' element={<Pokedex />}/>
+      
+
+
+    </Routes>
+</BrowserRouter>
+      )
+}
+
+export default App
---
+++ src/modules/Home.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='layout'>
+
+            <header className="header">
+                
+            </header>
+            <main className="main">
+                <img src="./src/images/imagepoke.png" alt="" />
+                <h1>¡Hola entrenador!</h1>
+                <h3>Para poder comenzar, dame tu nombre</h3>
+
+                <div>
+                    <input  className='input-home' type="text" placeholder='Tu nombre ...'/>
+                    <button className='button-home'>Comenzar</button>
+
+                </div>
+
+
+
+            </main>
+
+            <footer className="footer">
+                <div className='rectangle-red'>
+                    <div className='rectangle-black'>
+                    </div>
+                </div>
+                <div className='circle-big'>
+                    <div className='circle-small'>
+
+                    </div>
+                </div>
+            </footer>
+
+
+
+        </div>
+    );
+};
+
+export default Home;
